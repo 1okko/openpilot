8df70c4714f1344c163db4252eb69bc8ea58e1b7
1okko <136616964+1okko@users.noreply.github.com>
2026-04-05 19:23:37 +0800

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -75,6 +75,7 @@ def manager_init() -> None:
   params.put_bool("IsReleaseBranch", build_metadata.release_channel)
   params.put_bool("IsReleaseIqBranch", build_metadata.release_sp_channel)
   params.put("HardwareSerial", serial)
+  params.put_bool("IsMetric", True)  # 强制设置为公制 (km/h)
 
   # set dongle id
   reg_res = register(show_spinner=True)
